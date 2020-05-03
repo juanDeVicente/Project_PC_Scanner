97317ab6595995ab0d643372821a0f27536e01e8
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-03 19:18:03 +0200

    Ahora el text view de la version saca la version de la aplicación correctamente

--- app/src/main/java/com/projectpcscanner/ActivityWelcome.kt
+++ app/src/main/java/com/projectpcscanner/ActivityWelcome.kt
@@ -1,12 +1,14 @@
 package com.projectpcscanner
 
-import android.app.ActivityOptions
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ActivityWelcome : AppCompatActivity() {
@@ -18,10 +20,19 @@ class ActivityWelcome : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val nextButton: FloatingActionButton = findViewById(R.id.nextFloatingButton)
-        nextButton.setOnClickListener{ v: View? ->
-            val intent: Intent = Intent(this, ActivityLink::class.java)
+        nextButton.setOnClickListener{ _: View? ->
+            val intent = Intent(this, ActivityLink::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
+
+        try {
+            val info: PackageInfo = applicationContext.packageManager.getPackageInfo(packageName, 0)
+            val versionTextView: TextView = findViewById(R.id.versionTextView)
+            versionTextView.text = info.versionName
+        }
+        catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
     }
 }
