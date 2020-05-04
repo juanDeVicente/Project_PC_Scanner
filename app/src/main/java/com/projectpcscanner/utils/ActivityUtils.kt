e982f9ff69085c4cf41034b9ff9ccf66b4a3af2a
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-04 16:47:35 +0200

    Añadida utilidad para cerrar la aplicación desde cualquier punto

--- app/src/main/java/com/projectpcscanner/utils/ActivityUtils.kt
+++ app/src/main/java/com/projectpcscanner/utils/ActivityUtils.kt
@@ -3,8 +3,14 @@ package com.projectpcscanner.utils
 import android.app.Activity
 import android.view.Window
 import android.view.WindowManager
+import kotlin.system.exitProcess
 
 fun setActivityFullScreen(activity: Activity) {
     activity.requestWindowFeature(Window.FEATURE_NO_TITLE)
     activity.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
+
+fun exitApplication(activity: Activity) {
+    activity.finishAffinity()
+    exitProcess(0)
 }
