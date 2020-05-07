f86e59d82c1c6284a3adf8919d02eda706c6023c
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-07 17:43:23 +0200

    Añadido constructor que toma como parametro un JSONObject

--- app/src/main/java/com/projectpcscanner/models/StaticsModel.kt
+++ app/src/main/java/com/projectpcscanner/models/StaticsModel.kt
@@ -1,7 +1,20 @@
 package com.projectpcscanner.models
 
-class StaticsModel(val name: String, val currentValue: Float, val minValue: Float, val maxValue: Float, val relative: Boolean)
+import com.projectpcscanner.utils.toMap
+import org.json.JSONObject
+
+class StaticsModel(val name: String, val currentValue: Float, val minValue: Float, val maxValue: Float, val scalingFactor: Float, val measurementUnit: String, val details: Map<String, String>)
 {
+    constructor(jsonObject: JSONObject): this(
+        jsonObject.getString("name"),
+        jsonObject.getDouble("current_value").toFloat(),
+        jsonObject.getDouble("min_value").toFloat(),
+        jsonObject.getDouble("max_value").toFloat(),
+        jsonObject.getDouble("scaling_factor").toFloat(),
+        jsonObject.getString("measurement_unit"),
+        toMap(jsonObject.getJSONObject("details")) as Map<String, String>
+    )
+
     override fun equals(other: Any?): Boolean {
         if (other is StaticsModel)
             return other.name == name
