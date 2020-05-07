e647f238a2b097c6d06e59b57600d25a2065465f
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-07 17:53:46 +0200

    Añadido RecycleView para mostrar los detalles de la estadistica

---
+++ app/src/main/java/com/projectpcscanner/recycleviewadapters/DetailsRecycleViewAdapter.kt
@@ -0,0 +1,9 @@
+package com.projectpcscanner.recycleviewadapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class DetailsRecycleViewAdapter {
+
+    inner class MyViewHolder(private val view: View) : RecyclerView.ViewHolder(view)
+}
