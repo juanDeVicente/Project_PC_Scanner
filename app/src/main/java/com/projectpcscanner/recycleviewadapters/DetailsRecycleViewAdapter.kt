f528abc04c3ee00775c54d6ad04486667cd11b48
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-09 18:22:19 +0200

    Añadido recyclerview para mostrar los detalles de una estadistica en especifico

--- app/src/main/java/com/projectpcscanner/recycleviewadapters/DetailsRecycleViewAdapter.kt
+++ app/src/main/java/com/projectpcscanner/recycleviewadapters/DetailsRecycleViewAdapter.kt
@@ -1,9 +1,34 @@
 package com.projectpcscanner.recycleviewadapters
 
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.projectpcscanner.R
+import com.projectpcscanner.utils.getStringFromResourceByName
+import java.lang.Exception
 
-class DetailsRecycleViewAdapter {
 
-    inner class MyViewHolder(private val view: View) : RecyclerView.ViewHolder(view)
+class DetailsRecycleViewAdapter(private val details: Map<String, String>) : RecyclerView.Adapter<DetailsRecycleViewAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.detail_recycler_view_item, parent, false)
+        return MyViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return details.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val text = getStringFromResourceByName(holder.view.context, ArrayList(details.keys)[position]) + ":"
+        holder.view.findViewById<TextView>(R.id.detailNameTextView).text = text
+
+        holder.view.findViewById<TextView>(R.id.detailValueTextView).text = getStringFromResourceByName(holder.view.context, ArrayList(details.values)[position])
+
+    }
 }
