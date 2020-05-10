8f5720bda9fcb94b9bdc5ab619d9d0cea386498e
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-10 20:36:18 +0200

    Añadida funcion para lanzar el navegador desde una actividad

--- app/src/main/java/com/projectpcscanner/utils/ActivityUtils.kt
+++ app/src/main/java/com/projectpcscanner/utils/ActivityUtils.kt
@@ -1,6 +1,9 @@
 package com.projectpcscanner.utils
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.view.Window
 import android.view.WindowManager
 import kotlin.system.exitProcess
@@ -13,4 +16,17 @@ fun setActivityFullScreen(activity: Activity) {
 fun exitApplication(activity: Activity) {
     activity.finishAffinity()
     exitProcess(0)
+}
+
+fun openWebNavigator(activity: Activity, url: String) {
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    intent.setPackage("com.android.chrome")
+    try {
+        activity.startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        intent.setPackage(null)
+        activity.startActivity(intent)
+    }
+
 }
