ec7c321b6e139be31d9820bfac3f615a7ef51c29
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-05-18 17:57:27 +0200

    Eliminados imports inutiles

--- app/src/main/java/com/projectpcscanner/utils/StringUtils.kt
+++ app/src/main/java/com/projectpcscanner/utils/StringUtils.kt
@@ -1,6 +1,7 @@
 package com.projectpcscanner.utils
 
 import android.content.Context
+import android.util.Log
 import android.widget.TextView
 import com.projectpcscanner.R
 import java.lang.Exception
@@ -8,7 +9,7 @@ import java.lang.Exception
 fun getStringFromResourceByName(context: Context, name: String): String {
     val packageName: String = context.packageName
     return try {
-        context.getString(context.resources.getIdentifier(name, "string", packageName))
+        context.resources.getString(context.resources.getIdentifier(name, "string", packageName))
     }
     catch (e: Exception) {
         name
