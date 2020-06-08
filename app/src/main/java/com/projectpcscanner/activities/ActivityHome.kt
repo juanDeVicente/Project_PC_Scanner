a0caf9c1cc4dfe34c1618fefda066abbdfb9ee53
Juan de Vicente Tortosa <juan.vicente@live.u-tad.com>
2020-06-08 22:33:50 +0200

    Eliminados import inútiles

--- app/src/main/java/com/projectpcscanner/activities/ActivityHome.kt
+++ app/src/main/java/com/projectpcscanner/activities/ActivityHome.kt
@@ -7,7 +7,6 @@ import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
-import android.preference.PreferenceManager
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
--- app/src/main/java/com/projectpcscanner/utils/ActivityUtils.kt
+++ app/src/main/java/com/projectpcscanner/utils/ActivityUtils.kt
@@ -4,10 +4,6 @@ import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
-import android.view.View
-import android.view.Window
-import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import com.projectpcscanner.R
